4794fe1d6fbf27c736418a54ca1bb94e97092f19
trw321 <iceberg.tea33@gmail.com>
2026-05-07 14:42:24 -0400

    feat: add PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import posthog from "posthog-js";
 import App from "./App.tsx";
 import "./index.css";
+
+posthog.init("phc_udVxe34hMTHqcfzkfPno3idfPZGS7NeFwVGG9PgNT4t6", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only",
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
