2fd638581a28def4ae97f6c349dba47bb744139e
trw321 <iceberg.tea33@gmail.com>
2026-06-25 20:41:42 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import posthog from "posthog-js";
 import App from "./App.tsx";
 import "./index.css";
+import { UserPrefsProvider } from "./lib/UserPrefsContext";
 
 posthog.init("phc_udVxe34hMTHqcfzkfPno3idfPZGS7NeFwVGG9PgNT4t6", {
   api_host: "https://us.i.posthog.com",
@@ -12,8 +13,6 @@ posthog.init("phc_udVxe34hMTHqcfzkfPno3idfPZGS7NeFwVGG9PgNT4t6", {
     maskInputOptions: { password: true },
   },
 });
-
-import { UserPrefsProvider } from "./lib/UserPrefsContext";
 
 createRoot(document.getElementById("root")!).render(
   <UserPrefsProvider>
